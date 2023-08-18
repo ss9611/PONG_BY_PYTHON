b8ed88903c7dd0388ad0ab5091fc4df42b0fc803
SHAIK MAHABOOB ARSHAD <103995389+ss9611@users.noreply.github.com>
2023-08-18 20:05:05 +0530

    Pong.py

---
+++ Pong.py
@@ -0,0 +1,133 @@
+#Simple pong programs
+import turtle
+wn=turtle.Screen()
+wn.title("MY Pong Game")
+wn.bgcolor("black")
+wn.setup(width=800,height=600)
+wn.tracer(0)
+#scores
+score_a=0
+score_b=0
+#paddles
+
+#padddle_a:
+paddle_a=turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350,0)
+#paddle_b:
+paddle_b=turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350,0)
+#ball
+ball=turtle.Turtle()
+ball.speed(0)
+ball.color("white")
+ball.shape("circle")
+ball.penup()
+ball.goto(0,0)
+ball.dx=0.04
+ball.dy=0.04
+#pen
+pen=turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A:0 Player B:0",align="center",font=("Courier",24,"normal"))
+
+
+
+#functions to move the paddles
+def paddle_a_up():
+    y=paddle_a.ycor()
+    y+=20
+    paddle_a.sety(y)
+#listen to key borad using listen function
+#keyboard binding
+def paddle_a_down():
+    y=paddle_a.ycor()
+    y-=20
+    paddle_a.sety(y)
+def paddle_b_up():
+    y=paddle_b.ycor()
+    y+=20
+    paddle_b.sety(y)
+def paddle_b_down():
+    y=paddle_b.ycor()
+    y-=20
+    paddle_b.sety(y)
+
+wn.listen()
+wn.onkeypress
+wn.onkeypress(paddle_a_up,"w")
+wn.onkeypress(paddle_a_down,"s")
+wn.onkeypress(paddle_b_up,"Up")
+wn.onkeypress(paddle_b_down,"Down")
+
+
+
+
+
+
+# Main game loop
+while True:
+ wn.update()
+
+
+
+#move the ball
+ ball.setx(ball.xcor()+ball.dx)
+ ball.sety(ball.ycor()+ball.dy)
+ if ball.ycor()>290:
+   ball.sety(290)
+   ball.dy*=-1
+ if ball.ycor()<-290:
+   ball.sety(-290)
+   ball.dy*=-1 
+ if ball.xcor()>390:
+  
+   ball.goto(0,0)
+   ball.dx*=-1
+   score_a+=1
+   pen.clear()
+   pen.write("Player A:{} Player B:{}".format(score_a,score_b),align="center",font=("Courier",24,"normal"))
+   
+
+ if ball.xcor()<-390:
+   ball.goto(0,0)
+   ball.dx*=-1
+   score_b+=1
+   pen.clear()
+   pen.write("Player A:{} Player B:{}".format(score_a,score_b),align="center",font=("Courier",24,"normal"))
+ if ball.xcor()>340 and ball.xcor()<350 and ball.ycor()<paddle_b.ycor()+40 and ball.ycor()>paddle_b.ycor()-40:
+   ball.setx(340)
+   ball.dx*=-1
+ if (ball.xcor()<-340 and ball.xcor()>-350) and ball.ycor()<paddle_a.ycor()+40 and  ball.ycor()>paddle_a.ycor()-40:
+   ball.setx(340)
+   ball.dx*=-1
+ if paddle_a.ycor()>300:
+    ball.setx(340)
+ if (ball.xcor()<-340 and ball.xcor()>-350) and ball.ycor()<paddle_a.ycor()+40 and  ball.ycor()>paddle_a.ycor()-40:
+   ball.setx(340)
+   ball.dx*=-1   
+   #paddle limits
+ if paddle_a.ycor()>300:
+    paddle_a.sety(290)
+ if paddle_a.ycor()<-300:
+    paddle_a.sety(-290)   
+ if paddle_b.ycor()>300:
+    paddle_b.sety(290)
+ if paddle_b.ycor()<-300:
+    paddle_b.sety(-290)
+
+
+    
